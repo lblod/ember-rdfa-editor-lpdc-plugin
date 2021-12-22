435b8256f365c30235b6c6fddf8fcea915413cb4
Niels Vandekeybus <progster@gmail.com>
2021-12-22 11:33:07 +0100

    include services

--- addon/components/lpdc-public-service-modal.js
+++ addon/components/lpdc-public-service-modal.js
@@ -1,4 +1,23 @@
 import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+import { getOwner } from '@ember/application';
 
 export default class LpdcPublicServiceModalComponent extends Component {
+  @tracked serviceFilter;
+  services;
+  constructor() {
+    super(...arguments);
+    const config = getOwner(this).resolveRegistration('config:environment');
+    this.services = config.publicServices;
+  }
+
+  setFilter(filter) {
+    this.serviceFilter = filter;
+  }
+
+  get filteredServices() {
+    // TODO use filter
+    return this.services;
+  }
 }
