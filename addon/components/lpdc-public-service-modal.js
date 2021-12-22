765935bcd841d0e16d59578bb28ef11020e5ac26
Niels Vandekeybus <progster@gmail.com>
2021-12-22 12:39:01 +0100

    initial adding of articles

--- addon/components/lpdc-public-service-modal.js
+++ addon/components/lpdc-public-service-modal.js
@@ -2,6 +2,22 @@ import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 import { getOwner } from '@ember/application';
+import uuid from 'ember-rdfa-editor-lpdc-plugin/utils/uuid';
+
+const AUDIENCE_TO_URI_MAP = {
+  'Burger': {
+    uri: 'http://data.lblod.info/public-services/13643e20-631a-11ec-90d6-0242ac120003',
+    name: 'burgers'
+  },
+  'Onderneming': {
+    uri: 'http://data.lblod.info/public-services/136440dc-631a-11ec-90d6-0242ac120003' ,
+    name: 'ondernemingen'
+  },
+  'Organisatie': {
+    uri: 'http://data.lblod.info/public-services/13644212-631a-11ec-90d6-0242ac120003',
+    name: 'organisaties'
+  }
+};
 
 export default class LpdcPublicServiceModalComponent extends Component {
   @tracked serviceFilter;
@@ -19,5 +35,29 @@ export default class LpdcPublicServiceModalComponent extends Component {
   get filteredServices() {
     // TODO use filter
     return this.services;
+  }
+
+  @action
+  insertLPDCRules(service, params) {
+    const publicServiceURI = 'http://data.lblod.info/public-services/' + uuid();
+    const goal = `<strong>Doel</strong>
+           <div resource="${publicServiceURI}" typeof="http://purl.org/vocab/cpsv#PublicService">
+             <p property="dct:description">${service["Inhoud"]}</p>
+            </div>
+`;
+    const audiences = service["Doelgroep"].split(", ").map((s) => {
+      return {
+        title: AUDIENCE_TO_URI_MAP[s]["name"],
+        uri: AUDIENCE_TO_URI_MAP[s]["uri"]
+      };
+    });
+    const targetAudience = `
+           <strong>Doelgroep</strong>
+           <div resource="${publicServiceURI}" typeof="http://purl.org/vocab/cpsv#PublicService">
+<p>De doelgroepen van <span class="mark-highlight-manual">de/het</span> ${service["Titel"]} zijn ${audiences.map((x) => `<a property="http://data.europa.eu/m8g/http://data.europa.eu/m8g/isClassifiedBy" href="${x.uri}">${x.title}</a>`).join(',')}
+            </div>
+`;
+    this.args.controller.executeCommand('insert-article', goal);
+    this.args.controller.executeCommand('insert-article', targetAudience);
   }
 }
