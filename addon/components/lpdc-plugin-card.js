53729bc4e8e162d8f097ab9b18fae4a30137b230
Niels Vandekeybus <progster@gmail.com>
2021-12-22 11:15:21 +0100

    initial wiring of modal

--- addon/components/lpdc-plugin-card.js
+++ addon/components/lpdc-plugin-card.js
@@ -1,4 +1,16 @@
 import Component from '@glimmer/component';
-
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 export default class LpdcPluginCardComponent extends Component {
+  @tracked isModalOpen = false;
+
+  @action
+  openModal() {
+    this.isModalOpen = true;
+  }
+
+  @action
+  closeModal() {
+    this.isModalOpen = false;
+  }
 }
